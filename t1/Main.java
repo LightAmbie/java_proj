bc13e43d8a2da8c27ba38a75022f8f1e12c98c03
LightAmbie <lightambie@gmail.com>
2022-09-20 12:37:58 +0300

    Initial commit

---
+++ t1/Main.java
@@ -0,0 +1,28 @@
+package mirea.ru.lazarev_pavel.kvbo0121.pr4.t1;
+
+import mirea.ru.lazarev_pavel.kvbo0121.Season;
+
+public class Main {
+    public static void main(String[] args) {
+        Season myFav = Season.WINTER;
+
+        System.out.println("My favourite season: " + myFav);
+
+        iLike(myFav);
+        iLike(Season.SPRING);
+
+        System.out.println(myFav.getDescription());
+
+        myFav = Season.SUMMER;
+
+        System.out.println(myFav.getDescription());
+    }
+    public static void iLike(Season season){
+        switch (season){
+            case AUTUMN -> System.out.println("I like autumn");
+            case WINTER -> System.out.println("I like winter");
+            case SPRING -> System.out.println("I like spring");
+            case SUMMER -> System.out.println("I like summer");
+        }
+    }
+}
